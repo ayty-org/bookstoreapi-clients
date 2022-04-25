8aeae245cfdc8bf2885aa154b5b2e37cf651a6bd
Jackson Gomes <jacksonanderson1515@hotmail.com>
2022-04-25 12:42:35 -0300

    initial commit

---
+++ clients/src/main/java/br/com/bookstoreapi/clients/BookstoreClientsApplication.java
@@ -0,0 +1,13 @@
+package br.com.bookstoreapi.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bookstoreapi/clients/BookstoreClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bookstoreapi.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
