d816f329110af115630199dd129c16c49a11d2ec
Jackson Gomes <jacksonanderson1515@hotmail.com>
2022-05-09 10:50:18 -0300

    cloud gateway configured

--- src/main/java/br/com/bookstoreapi/clients/BookstoreClientsApplication.java
+++ src/main/java/br/com/bookstoreapi/clients/BookstoreClientsApplication.java
@@ -3,8 +3,11 @@ package br.com.bookstoreapi.clients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,5 +18,6 @@ public class BookstoreClientsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreClientsApplication.class, args);
 	}
+
 
 }
