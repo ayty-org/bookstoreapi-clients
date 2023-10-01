5665e800385dc47b6568ca926f8d0ce228ae2b29
JacksonAGomes <jacksonanderson1515@hotmail.com>
2023-10-01 09:55:11 -0300

    changing configuration

--- src/main/java/br/com/bookstoreapi/clients/exception/ApiExceptionHandler.java
+++ src/main/java/br/com/bookstoreapi/clients/exception/ApiExceptionHandler.java
@@ -4,12 +4,12 @@ import feign.FeignException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApiExceptionHandler {
 
 
